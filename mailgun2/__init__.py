b7812fe4cbd99649b8fb26aed56f7bf9a5b9cc18
Albert Wang <git@albertyw.com>
2020-01-26 16:06:15 -0800

    Make setup.py not depend on the code being set up

--- mailgun2/__init__.py
+++ mailgun2/__init__.py
@@ -1,8 +1,5 @@
 from __future__ import absolute_import
 
-__title__ = 'mailgun2'
-__version__ = '1.1.0'
-__author__ = 'Albert Wang'
-__license__ = 'Apache 2.0'
+from .__version__ import __version__  # noqa: F401
 
 from .mailgun import Mailgun  # noqa: F401
--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python
-from setuptools import setup
-import mailgun2
+# -*- coding: utf-8 -*-
 
-try:
-    readme = open("README.rst")
-    long_description = str(readme.read())
-finally:
-    readme.close()
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-download_url = ("https://github.com/albertyw/python-mailgun2/"
-                "archive/%s.tar.gz") % mailgun2.__version__
+
+# Get the long description from the README file
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+about = {}
+with open(path.join(here, 'mailgun2', '__version__.py')) as f:
+    exec(f.read(), about)
+
 
 setup(
-    name=mailgun2.__title__,
-    packages=[mailgun2.__title__],
-    version=mailgun2.__version__,
+    name='mailgun2',
+    packages=find_packages(exclude=['tests']),
+    version=about['__version__'],
     description='A python client for Mailgun API v2',
     long_description=long_description,
-    author=mailgun2.__author__,
+    author='Albert Wang',
     author_email='git@albertyw.com',
     url='https://github.com/albertyw/python-mailgun2',
-    download_url=download_url,
     keywords=['mailgun', 'email'],
     install_requires=[
         'requests>=2.6,<3.0',
