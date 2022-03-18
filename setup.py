2c7ea6ab3a804d2ea428ca0d9b776e73fd7a7eec
Albert Wang <git@albertyw.com>
2022-03-18 01:07:52 -0400

    Update python versions supported

--- setup.py
+++ setup.py
@@ -45,10 +45,10 @@ setup(
         'Intended Audience :: Telecommunications Industry',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Communications :: Email',
         'Topic :: Software Development :: Libraries :: Python Modules'
         ],
